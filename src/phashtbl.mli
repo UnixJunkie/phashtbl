1ca3088eb08127fcf498b579610c2cb6e27c192e
Francois BERENGER <ligand@free.fr>
2018-05-08 10:20:00 +0900

    added ocamldoc in .mli

--- src/phashtbl.mli
+++ src/phashtbl.mli
@@ -1,37 +1,105 @@
-(* a hash table stored on disk (e.g. because it would not fit in RAM) *)
+(** {2 Persistent hash tables stored on disk using dbm under the carpet.} *)
 
 type filename = string
 
-(* val find: t -> string -> 'b
-   we have to marshal and unmarshal values *)
-module StrKeyToGenVal : sig
+(** {4 Polymorphic keys and values.} *)
 
-  type t
+module GenKeyToGenVal : sig
 
-  (* to create and populate a new DB *)
-  val open_new: filename -> t
+  type ('a, 'b) t
 
-  (* to open an existing one *)
-  val open_existing: filename -> t
+  (** [open_new filename] creates a new persistent hashtbl. *)
+  val open_new: filename -> ('a, 'b) t
 
-  val close: t -> unit
+  (** [open_existing filename] opens an existing persistent hashtbl
+      for reading and writing. *)
+  val open_existing: filename -> ('a, 'b) t
 
-  val mem: t -> string -> bool
+  (** [close pht] closes the previously opened [pht]. *)
+  val close: ('a, 'b) t -> unit
 
-  val add: t -> string -> 'b -> unit
+  (** [mem pht key] checks if [key] is bound in [pht]. *)
+  val mem: ('a, 'b) t -> 'a -> bool
 
-  val replace: t -> string -> 'b -> unit
+  (** [add pht key value] binds [key] to [value] in [pht]. *)
+  val add: ('a, 'b) t -> 'a -> 'b -> unit
 
-  val remove: t -> string -> unit
+  (** [replace pht key value] TODO: check Dbm's doc *)
+  val replace: ('a, 'b) t -> 'a -> 'b -> unit
 
-  val find: t -> string -> 'b
+  (** [remove pht key] removes [key] and its bound value from [pht]. *)
+  val remove: ('a, 'b) t -> 'a -> unit
 
-  val iter: (string -> 'b -> unit) -> t -> unit
+  (** [find pht key] finds the value bound to [key] in [pht].
+      TODO: check Dbm's doc. *)
+  val find: ('a, 'b) t -> 'a -> 'b
 
-  (* WARNING: only use on a small table *)
-  val fold: (string -> 'b -> 'c -> 'c) -> t -> 'c -> 'c
+  (** [iter f pht] calls [f key value] on each [key]-[value] binding from [pht]. *)
+  val iter: ('a -> 'b -> unit) -> ('a, 'b) t -> unit
+
+  (** [fold f pht init] folds [f] over [pht]
+      with [init] as the initial accumulator. *)
+  val fold: ('a -> 'b -> 'c -> 'c) -> ('a, 'b) t -> 'c -> 'c
 
 end
+
+(** {4 String keys and polymorphic values.} *)
+
+module StrKeyToGenVal : sig
+
+  type 'b t
+
+  val open_new: filename -> 'b t
+
+  val open_existing: filename -> 'b t
+
+  val close: 'b t -> unit
+
+  val mem: 'b t -> string -> bool
+
+  val add: 'b t -> string -> 'b -> unit
+
+  val replace: 'b t -> string -> 'b -> unit
+
+  val remove: 'b t -> string -> unit
+
+  val find: 'b t -> string -> 'b
+
+  val iter: (string -> 'b -> unit) -> 'b t -> unit
+
+  val fold: (string -> 'b -> 'c -> 'c) -> 'b t -> 'c -> 'c
+
+end
+
+(** {4 Polymorphic keys and string values.} *)
+
+module GenKeyToStrVal : sig
+
+  type 'a t
+
+  val open_new: filename -> 'a t
+
+  val open_existing: filename -> 'a t
+
+  val close: 'a t -> unit
+
+  val mem: 'a t -> 'a -> bool
+
+  val add: 'a t -> 'a -> string -> unit
+
+  val replace: 'a t -> 'a -> string -> unit
+
+  val remove: 'a t -> 'a -> unit
+
+  val find: 'a t -> 'a -> string
+
+  val iter: ('a -> string -> unit) -> 'a t -> unit
+
+  val fold: ('a -> string -> 'c -> 'c) -> 'a t -> 'c -> 'c
+
+end
+
+(** {4 String keys and values.} *)
 
 module StrKeyToStrVal : sig
 
@@ -56,57 +124,5 @@ module StrKeyToStrVal : sig
   val iter: (string -> string -> unit) -> t -> unit
 
   val fold: (string -> string -> 'c -> 'c) -> t -> 'c -> 'c
-
-end
-
-module GenKeyToGenVal : sig
-
-  type t
-
-  val open_new: filename -> t
-
-  val open_existing: filename -> t
-
-  val close: t -> unit
-
-  val mem: t -> 'a -> bool
-
-  val add: t -> 'a -> 'b -> unit
-
-  val replace: t -> 'a -> 'b -> unit
-
-  val remove: t -> 'a -> unit
-
-  val find: t -> 'a -> 'b
-
-  val iter: ('a -> 'b -> unit) -> t -> unit
-
-  val fold: ('a -> 'b -> 'c -> 'c) -> t -> 'c -> 'c
-
-end
-
-module GenKeyToStrVal : sig
-
-  type t
-
-  val open_new: filename -> t
-
-  val open_existing: filename -> t
-
-  val close: t -> unit
-
-  val mem: t -> 'a -> bool
-
-  val add: t -> 'a -> string -> unit
-
-  val replace: t -> 'a -> string -> unit
-
-  val remove: t -> 'a -> unit
-
-  val find: t -> 'a -> string
-
-  val iter: ('a -> string -> unit) -> t -> unit
-
-  val fold: ('a -> string -> 'c -> 'c) -> t -> 'c -> 'c
 
 end
